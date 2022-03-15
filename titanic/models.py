967c254aefc18b827fd02abd3dda27495e0c6ccf
coder-jinyeong <78059103+coder-jinyeong@users.noreply.github.com>
2022-03-15 17:52:21 +0900

    22-03-15

---
+++ titanic/models.py
@@ -0,0 +1,14 @@
+from icecream import ic
+from context.models import Model
+from context.domains import Dataset
+
+
+class TitanicModel(object):
+    def __init__(self, train_fname, test_fname):
+        self.model = Model()
+        self.dataset = Dataset()
+        self.train = self.model.new_model(train_fname)
+        self.test = self.model.new_model(test_fname)
+        # id 추출
+        ic(f'트레인 컬럼 {self.train.columns}')
+        ic(f'트레인 헤드 {self.train.head()}')
